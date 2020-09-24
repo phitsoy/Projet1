acd36adc1f2a2ec262fd804e6c195094c531e83c
phitsoy <71102006+phitsoy@users.noreply.github.com>
2020-09-24 12:56:33 -0400

    Version finale

--- poutre_rect.py
+++ poutre_rect.py
@@ -1,7 +1,7 @@
 # Initialisation des variables
 
 F = 10000  # en N
-E = 210  # en GPa = 10^3 N/mm^2
+#E = 210  # en GPa = 10^3 N/mm^2
 E= 210*10**3 #en N/mm^2
 L = 100  # en mm
 b = 10  # en mm
--- poutre_section.py
+++ poutre_section.py
@@ -1,6 +1,7 @@
 # Initialisation des variables
 F = 10000  # en N
-E = 210  # en GPa = 10^3 N/mm^2
+#E = 210  # en GPa = 10^3 N/mm^2
+E = 210*10**3 # en N/mm^2
 L = 100  # en mm
 
 # poutre rectangulaire
@@ -20,3 +21,4 @@ d = 5  # en mm
 
 # Calcul de la section optimale
 
+print("Le type de section minimisant la déformation maximale est " + type + ", avec une déformation de " + str(round(delta_max, 2)) +" mm")
