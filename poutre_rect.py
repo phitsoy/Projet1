f4d47095488a073b80174159082b304e17e6b8a8
phitsoy <71102006+phitsoy@users.noreply.github.com>
2020-09-24 12:43:45 -0400

    Version finale

--- poutre_rect.py
+++ poutre_rect.py
@@ -2,15 +2,18 @@
 
 F = 10000  # en N
 E = 210  # en GPa = 10^3 N/mm^2
+E= 210*10**3 #en N/mm^2
 L = 100  # en mm
 b = 10  # en mm
 h = 20  # en mm
 
 # Calcul de l'inertie
 
-I = 
+I=(b*h**3)/12
+
 
 # Calcul de la déformation maximale
 
-delta_max = 
+delta_max =(F*L**3)/(3*E*I)
+print(delta_max)
 
